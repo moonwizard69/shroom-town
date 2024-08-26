aa00bb97eea73635752ef9d2f18e995cad5979c3
Matt Cousineau <mjcousineau1@gmail.com>
2024-08-26 00:39:23 -0400

    Adding mutations and item updating

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,20 +1,66 @@
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+import Router from 'next/router';
+import DisplayError from './ErrorMessage.js';
 import useForm from '../lib/useForm';
 import Form from './styles/Form.js';
+import { ALL_PRODUCTS_QUERY } from './Products.js';
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    # we name and past arguments to this flexible mutation in order to pass in vars
+    # this is graphql notation - exclamation on data type means required
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
 
 export default function CreateProduct() {
   const { inputs, handleChange, clearForm, resetForm } = useForm({
     image: '',
-    name: 'nice',
-    price: 883,
-    description: 'this is nice',
+    name: '',
+    price: '',
+    description: '',
   });
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+      refetchQueries: [{ query: ALL_PRODUCTS_QUERY }],
+    }
+  );
   return (
     <Form
-      onSubmit={(e) => {
+      onSubmit={async (e) => {
         e.preventDefault();
+        // submit the input fields to the backend
+        const res = await createProduct();
+        clearForm();
+        // Go to product page after creation
+        Router.push({
+          pathname: `/product/${res.data.createProduct.id}`,
+        });
       }}
     >
-      <fieldset>
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="image">
           Image
           <input
@@ -57,12 +103,7 @@ export default function CreateProduct() {
             onChange={handleChange}
           />
         </label>
-        <button type="button" onClick={clearForm}>
-          Clear Form
-        </button>
-        <button type="button" onClick={resetForm}>
-          Reset Form
-        </button>
+        <button type="submit">+ Add Product</button>
       </fieldset>
     </Form>
   );
