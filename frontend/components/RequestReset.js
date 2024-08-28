60dff47dd30978026e9c51e587873ef894982c50
Matt Cousineau <mjcousineau1@gmail.com>
2024-08-28 11:27:24 -0400

    Adding password reset form and token generation

---
+++ frontend/components/RequestReset.js
@@ -0,0 +1,60 @@
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+import useForm from '../lib/useForm';
+import Form from './styles/Form';
+import { CURRENT_USER_QUERY } from './User';
+import DisplayError from './ErrorMessage';
+
+const REQUEST_RESET_MUTATION = gql`
+  mutation REQUEST_RESET_MUTATION($email: String!) {
+    sendUserPasswordResetLink(email: $email) {
+      code
+      message
+    }
+  }
+`;
+export default function RequestReset() {
+  const { inputs, handleChange, resetForm } = useForm({
+    email: '',
+  });
+  const [signup, { data, loading }] = useMutation(REQUEST_RESET_MUTATION, {
+    variables: inputs,
+    // refetchQueries: [{ query: CURRENT_USER_QUERY }],
+  });
+
+  async function handleSubmit(e) {
+    e.preventDefault(); // STOP FORM FROM SUBMITTING
+    await signup().catch(console.error);
+    resetForm();
+  }
+
+  const error =
+    data?.authenticateUserWithPassword?.__typename ===
+    'UserAuthenticationWithPasswordFailure'
+      ? data?.authenticateUserWithPassword
+      : undefined;
+
+  return (
+    <Form method="POST" onSubmit={handleSubmit}>
+      <h2>Request a Password Reset</h2>
+      <DisplayError error={error} />
+      <fieldset>
+        {data?.sendUserPasswordResetLink === null && (
+          <p>Check yur email for a password reset link.</p>
+        )}
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email Address"
+            autoComplete="email"
+            value={inputs.email}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Request Reset</button>
+      </fieldset>
+    </Form>
+  );
+}
